7022bc7fcd8cfbeaebe5692ed5585881c8d9717b
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-10-11 20:31:21 +0300

    basic react-native

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native'
+import GoalInput from './components/GoaInput'
+import GoalItem from './components/GoalItem'
 
 export default function App() {
+  const [lifeGoals, setLifeGoals] = useState([])
+  const [isAddmode, setIsAddMode] = useState(false)
+  const addGoalHandler = (goalTitle) => {
+    setLifeGoals((currentLifeGoals) => [
+      ...currentLifeGoals,
+      { id: Math.random().toString(), value: goalTitle },
+    ])
+    setIsAddMode(false)
+  }
+
+  const removeGoalHandler = (goalId) => {
+    setLifeGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== goalId)
+    })
+  }
+  const cancelHandler = () => {
+    setIsAddMode(false)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Add new goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        onCancel={cancelHandler}
+        visible={isAddmode}
+        onAddGoal={addGoalHandler}
+      />
+
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={lifeGoals}
+        renderItem={(itemData) => (
+          <GoalItem
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
-});
+})
